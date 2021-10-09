838eb2ec47aec69663c57a2a36048f7e43cbd479
ezzeldenWork <ezzelden.work@gmail.com>
2021-10-09 20:14:22 +0200

    fix paltte route

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import {genetatrPalette} from "./colorHelpers";
 import {Route, Switch} from "react-router";
 
 export class App extends Component {
+  findPaltte(id) {
+   return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
   render() {
     return (
       <Switch>
         <Route exact path="/" />
-        <Route exact path="/palette/:id" />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={genetatrPalette(
+                this.findPaltte(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
       </Switch>
       // <div>
       //   <Palette palette={genetatrPalette(seedColors[3])} />
