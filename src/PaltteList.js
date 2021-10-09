553f708531b3e671df74fea347204e1a412dbf93
ezzeldenWork <ezzelden.work@gmail.com>
2021-10-10 01:20:33 +0200

    add  component  style

--- src/PaltteList.js
+++ src/PaltteList.js
@@ -1,15 +1,20 @@
 import React, {Component} from "react";
-import { Link } from "react-router-dom"; 
+import {Link} from "react-router-dom";
+import MiniPalette from "./MiniPalette";
 
 export class PaltteList extends Component {
   render() {
     const {palttes} = this.props;
     return (
       <div>
+        <MiniPalette />
         <h1>React Color</h1>
         {palttes.map((paletteMap) => (
           <p>
-          <Link to={`/palette/${paletteMap.id}`}> {paletteMap.paletteName} </Link></p>
+            <Link to={`/palette/${paletteMap.id}`} key={paletteMap.id}>
+              {paletteMap.paletteName}
+            </Link>
+          </p>
         ))}
       </div>
     );
