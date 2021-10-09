8752dbbecc03581bfb421bb6b6e34bc4f6ad2214
ezzeldenWork <ezzelden.work@gmail.com>
2021-10-09 20:07:48 +0200

    add Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, {Component} from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import {genetatrPalette} from "./colorHelpers";
+import {Route, Switch} from "react-router";
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <Palette palette={genetatrPalette(seedColors[2])} />
-      </div>
+      <Switch>
+        <Route exact path="/" />
+        <Route exact path="/palette/:id" />
+      </Switch>
+      // <div>
+      //   <Palette palette={genetatrPalette(seedColors[3])} />
+      // </div>
     );
   }
 }
